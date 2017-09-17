8a84b17914fc09d88ff941a8bb0166d5039a49f8
Emily Morgan <emorgan05@gmail.com>
2017-09-17 16:05:04 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,6 @@
 def square_array(array)
-  new_numbers = []
-
-  array.each do |number|
-    new_numbers.push(number ** 2)
+  new_numbers = array.collect do |number|
+    number ** 2
   end
 
   return new_numbers
